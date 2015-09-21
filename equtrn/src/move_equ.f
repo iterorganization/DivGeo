37b3731ebf4871f41ee6826e39ba278b1049b5cd
Stefano Carli <stefano.carli@polito.it>
2015-09-21 09:36:42 +0200

    introducing move_equ program for shifting of the equilibrium

---
+++ equtrn/src/move_equ.f
@@ -0,0 +1,43 @@
+!> Shift of the equilibrium up-down or left-right by a specified increment
+!>
+!> arg1: input equilibrium file name
+!>
+!> arg2: output equilibrium file name
+!>
+!> shift is read in from standard input
+!>
+!> \version  18.09.15 13:13	Stefano Carli
+
+      program move_equ
+c
+c  version : 30.01.96 21:58
+c
+c=====================================================
+c*** Shift of the magnetic flux by a specified increment
+c=====================================================
+      parameter (ngpr=257, ngpz=257)
+      real*8 pfm(ngpr,ngpz),rgr(ngpr),zgr(ngpz)
+      real*8 rcntc,psilim,btorc,shiftr,shiftz
+c=====================================================
+c
+      call open_files('the increment is taken from the standard input')
+
+      call rdeqdg(1,ngpr,ngpz,iret,nr,nz,btorc,rcntc,rgr,zgr,pfm)
+      if(iret.ne.0) then
+          print *,'==== move_equ: error in rdeqdg. iret =',iret
+          stop
+      end if
+c
+      print *,'desired shift along r in meters ='
+      read *,shiftr
+      print *,'desired shift along z in meters ='
+      read *,shiftz
+      rgr(1:nr)=rgr(1:nr)+shiftr
+      zgr(1:nz)=zgr(1:nz)+shiftz
+      psilim=0.
+      call wreqdg(2,ngpr,ngpz,iret,nr,nz,psilim,btorc,rcntc,rgr,zgr,pfm)
+      if(iret.ne.0) then
+          print *,'==== move_equ: error in wreqdg. iret = ',iret
+      end if
+c
+      end
