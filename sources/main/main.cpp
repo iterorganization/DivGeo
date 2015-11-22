c9baff30699151f5f09ca2d5393a48e27c5baa4c
Mikhail Buinevich <mikhail.buinevich@gmail.com>
2015-11-22 13:56:16 +0300

    Two digit exponent in Windows fixed.

--- sources/main/main.cpp
+++ sources/main/main.cpp
@@ -18,11 +18,12 @@ int main(int argc, char *argv[])
   mw.show();
   /* mw will delete the console in dtor */
 
-    /* To fix problem of decimal comma */
+  /* To fix problem of decimal comma */
   setlocale(LC_ALL, "C");
-#ifdef QT_OS_WIN
-  /* Use POSIX's format 0.0E-00 (not MS's 0.0E-000). Build with -posix option */
-  _putenv( "PRINTF_EXPONENT_DIGITS=2" );
+#ifdef Q_OS_WIN
+  /* Use POSIX's format 0.0E-00 (not MS's 0.0E-000 -- this is C++ Standard violation). Build with -posix option */
+  // _putenv( "PRINTF_EXPONENT_DIGITS=2" ); -- not works for now
+  _set_output_format( _TWO_DIGIT_EXPONENT ); //1409
 #endif
 
   return a.exec();
