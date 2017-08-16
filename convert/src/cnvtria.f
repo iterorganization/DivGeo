d7d121ed9c74b00efda81c5472d3b6747266419d
Xavier Bonnin <Xavier.Bonnin@iter.org>
2017-08-16 11:26:50 +0200

    Fixes to triageom

--- convert/src/cnvtria.f
+++ convert/src/cnvtria.f
@@ -26,23 +26,23 @@ c*** Read the data from the input file
 c
 c*** read *.npco_char
       read(*,*) npoint
-      write(0,*) 'npoint', npoint
+      write(0,'(a,t15,i6)') 'npoint', npoint
       allocate(px(npoint),py(npoint))
       read(*,*) px
       read(*,*) py
       write(0,*) '    reading *.npco_char is finished'
 c*** read *.elemente 
       read(*,*) ntria
-      write(0,*) 'ntria', ntria 
+      write(0,'(a,t15,i6)') 'ntria', ntria
       allocate(tri(3,3*ntria),pairs(2,3*ntria))
-      do i=1,ntria   
+      do i=1,ntria
        read(*,*) j,(tri(l,i),l=1,3)
-      end do 
+      end do
       write(0,*) '    reading *.element is finished'
 c----------------------------------------------------------------------
 c*** find pairs (matched nodes)  
-      k=0  
-      do i=1,ntria          
+      k=0
+      do i=1,ntria
 c*** examin each combination of nodes in each triangle
        do i1=1,3
         i2=mod(i1,3)+1
@@ -50,22 +50,22 @@ c*** examin each combination of nodes in each triangle
         j2=max(tri(i1,i),tri(i2,i))
         do j=1,k
 c*** chech if this pair is already present
-         if(pairs(1,j).EQ.j1.AND.pairs(2,j).EQ.j2) goto 11        
-        end do 
+         if(pairs(1,j).EQ.j1.AND.pairs(2,j).EQ.j2) goto 11
+        end do
 c*** add pair
         k=k+1
         pairs(1,k)=j1
         pairs(2,k)=j2
    11   continue
        end do
-      end do 
-      npair=k  
+      end do
+      npair=k
 c*** output template (coordinate of triangle edges in mm)              
-      do i=1,npair 
+      do i=1,npair
         write(*,*) 10.*px(pairs(1,i)),10.*py(pairs(1,i))
         write(*,*) 10.*px(pairs(2,i)),10.*py(pairs(2,i))
         write(*,*)
-      end do 
+      end do
       write(0,*) '    writing template is finished'
       deallocate(px,py,tri,pairs)
       end
