c7f3e42683fbd423d6ae2d63fef9eeb76aaa1ac2
Xavier Bonnin <Xavier.Bonnin@iter.org>
2023-11-15 16:58:29 +0100

    First part of moving wall IDS write-up from DivGeo to DD/3.39.0 standard

--- equtrn/src/eqdim.f90
+++ equtrn/src/eqdim.f90
@@ -32,6 +32,55 @@ include 'mathematical_constants.f90'              ! IGNORE
   parameter (pi=3.141592653589793_R8)
 #endif
 #endif
+
+  public :: int2str, real2str
+
+  contains
+
+  function int2str(intval) result(string)
+    integer, intent(in) :: intval
+    character(len=2-max(sign(1,intval),0)+max( &
+         min(abs(intval)/10**1,1)*1, &
+         min(abs(intval)/10**2,1)*2, &
+         min(abs(intval)/10**3,1)*3, &
+         min(abs(intval)/10**4,1)*4, &
+         min(abs(intval)/10**5,1)*5, &
+         min(abs(intval)/10**6,1)*6, &
+         min(abs(intval)/10**7,1)*7, &
+         min(abs(intval)/10**8,1)*8, &
+         min(abs(intval)/10**9,1)*9) ) :: string
+    integer :: absn,j,k,is
+
+    absn = abs(intval)
+    if ( absn == intval ) then
+            is = 1
+    else
+            is = 2
+            string(1:1) = "-"
+    end if
+    do j=len(string),is,-1
+            k = modulo(absn,10)+1
+            string(j:j) = "0123456789"(k:k)
+            absn = absn / 10
+    end do
+    return
+
+  end function int2str
+
+  !> Write a real to a string with 15 digits
+  !> +1.23456789012345E000
+  function real2str(realval) result(string)
+    character(*), parameter :: SAMPLE = '+1.23456789012345E000'
+    !                                     0.10000000000000E+03
+    !                                    -0.10000000000000E+03
+    real(kind=R8), intent(in) :: realval
+    character(len(SAMPLE)) :: string
+
+    write( string, '(es21.14)' ) realval
+    return
+
+  end function real2str
+
   end module eqdim
 
 !!!Local Variables:
